157a3d19f4beba5933454436a17c731d1c5aeeb9
Augusto Ostapechen <augusto_ostapechen@hotmail.com>
2020-03-02 02:06:07 -0300

    Tabela e operações p/ o agendamento de serviços

--- src/routes.js
+++ src/routes.js
@@ -6,18 +6,23 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
-routes.post('/users', UserController.store);
-routes.put('/users', authMiddleware, UserController.update);
-
 routes.post('/sessions', SessionController.store);
+routes.post('/users', UserController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
