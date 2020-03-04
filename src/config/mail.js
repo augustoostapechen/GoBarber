33b859f8efa679a4a83c9042212117f4984ef72d
Augusto Ostapechen <augusto_ostapechen@hotmail.com>
2020-03-04 07:57:19 -0300

    Adicionado envio de email com Mailtraper

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'c39ae6179bf5c5',
+    pass: 'c91dcf393522e1',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
